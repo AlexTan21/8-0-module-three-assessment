1be8d1c613c3cfdee55de13a791515c68f375c3b
Alex Tan <alexmyot@yahoo.com>
2021-12-16 17:36:49 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
+import Home from "./components/Home.js";
+import Locations from "./components/Locations.js";
+import Movies from "./components/Movies.js";
+import Navbar from "./components/Navbar.js";
+import People from "./components/People.js";
 
 function App() {
   return (
     <div className="app">
-      <main>
-        <h1>Hello, world!</h1>
-      </main>
+      <Navbar />
+      <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/people" component={People} />
+          <Route path='/locations' component={Locations} />
+        </Switch>
     </div>
   );
 }
